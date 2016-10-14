6d5ffc56aa7c64bd76078f0c2ec362deef174db5
Champ Clark III <champ@dev.quadrantsec.com>
2016-10-14 17:30:57 +0000

    Minor changes to take into account cid++ at startup.

--- src/output-plugins/spo_database.c
+++ src/output-plugins/spo_database.c
@@ -102,6 +102,10 @@ static const char* FATAL_NO_SUPPORT_2 =
 
 #include "output-plugins/spo_database.h"
 
+
+u_int8_t first_pass = 0; 
+
+
 void DatabaseCleanSelect(DatabaseData *data)
 {
     
@@ -363,6 +367,7 @@ u_int32_t SynchronizeEventId(DatabaseData *data)
     }
     
     data->cid++;
+    first_pass = 1; 
 
 
     if( UpdateLastCid(data, data->sid, data->cid) < 0 )
@@ -5447,6 +5452,8 @@ int dbProcessExtraData( DatabaseData *data, void *event, u_int32_t event_type )
     char *packet_data = NULL, *packet_data_not_escaped = NULL, *insert0 = NULL;
     u_char *extraData = NULL;
 
+    int new_cid; 
+
     if (data == NULL)
 	{   
 	return 1;
@@ -5510,6 +5517,21 @@ int dbProcessExtraData( DatabaseData *data, void *event, u_int32_t event_type )
         
         packet_data = snort_escape_string(packet_data_not_escaped, data);
 
+	/* This factors in the very first data->cid++ that happens on startup.  This way, the extra
+   	   data doesn't mistakenly reference the previous run! - Champ Clark III 11/14/2016 */
+
+	if ( first_pass == 1 ) 
+	   { 
+
+		new_cid = data->cid;
+
+	   } else { 
+
+		new_cid = data->cid-1; 
+		first_pass = 0; 
+	   }
+
+
         if (data->dbtype_id == DB_ORACLE) {
             /* Oracle field BLOB type case. We append unescaped
              * packet_payload data after query, which later in Insert()
@@ -5520,7 +5542,7 @@ int dbProcessExtraData( DatabaseData *data, void *event, u_int32_t event_type )
                     "extra (sid,cid,type,datatype,len,data) "
                     "VALUES (%u,%u,%u,%u,%u,:1)|%s",
                    data->sid,
-                   data->cid-1,			
+                   new_cid,
                    ntohl(extraEvent->type),
                    ntohl(extraEvent->data_type),
                    len,
@@ -5539,7 +5561,7 @@ int dbProcessExtraData( DatabaseData *data, void *event, u_int32_t event_type )
                     "extra (sid,cid,type,datatype,len,data) "
                     "VALUES (%u,%u,%u,%u,%u,'%s')",
                     data->sid,
-                    data->cid-1,
+                    new_cid,
                     ntohl(extraEvent->type),
                     ntohl(extraEvent->data_type),
                     len,
